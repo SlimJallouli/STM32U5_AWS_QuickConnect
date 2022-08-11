b771ef370c8d63d3008686d7ac39c69428454a2c
AlnurElberier <a.elberier@gmail.com>
2022-08-11 13:03:18 -0700

    add files

---
+++ utils/openDashboard.py
@@ -0,0 +1,40 @@
+import os, sys
+import getopt
+
+
+HELP = ['openDashboard.py options:', 
+        '\n\t-h or --help for help',
+        '\n\t--key-id=[access key]', 
+        '\n\t--secret-key=[secret key]', 
+        '\n\t--device-id=[device name]']
+
+
+def main(argv):
+
+    key = secretKey = deviceName = ''
+
+    # Collect Parameters from command line
+    try:
+        opts, args = getopt.getopt(argv,"h", ["help", "key-id=", "secret-key=", "device-id="])
+    except getopt.GetoptError:
+        sys.exit()
+
+    for opt, arg in opts:
+        if opt in ("-h", "--help"):
+            print(*HELP)
+            sys.exit()
+
+        elif opt in ("--key-id"):
+            key = arg
+
+        elif opt in ("secret-key="):
+            secretKey = arg
+
+        elif opt in ("device-id="):
+            deviceName = arg
+
+       
+        os.system("start \"\" " + "http://stm32u5-aws-dashboard.s3-website-us-east-1.amazonaws.com/?KEY_ID="+ key + "&SECRET_KEY=" + secretKey + "&DeviceID=" + deviceName)
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
