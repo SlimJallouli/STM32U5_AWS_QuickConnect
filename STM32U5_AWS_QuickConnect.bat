966475a41a82fa2672ef929d2f7090e6dc7aa61e
AlnurElberier <a.elberier@gmail.com>
2022-08-11 13:02:07 -0700

    add parameters

--- STM32U5_AWS_QuickConnect.bat
+++ STM32U5_AWS_QuickConnect.bat
@@ -1,3 +1,11 @@
+set name=techTour
+set ssid=MySSID
+set pswd=MyPSWD
+set key=test
+set secretKey=test
+
 call python utils\flash.py
-call python utils\provision.py -i -v
+::reset ssid python scrippt
+call python utils\provision.py --thing-name %name% --wifi-ssid %ssid% --wifi-dredential %pswd% -v
+call python utils\openDashboard.py --device-id=%name% --key-id=%key% --secret-key=%secretKey%
 call python utils\readSerial.py
