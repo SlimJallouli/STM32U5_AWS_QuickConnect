6a9d5b3574cb6110d7040d918b22f5d70403ca53
SlimJallouli <slim.jallouli@st.com>
2022-09-06 15:53:46 -0700

    bin file path. open the amplify dashboard

--- STM32U5_AWS_QuickConnect.py
+++ STM32U5_AWS_QuickConnect.py
@@ -5,24 +5,30 @@ from tokenize import Name
 from utils.getDeviceName import *
 import getopt
 import getpass
+import platform
 
-SSID = 'STTestN750'
-PSWD = '9a1v2r73027'
+SSID = 'st_iot_demo'
+PSWD = 'stm32u585'
 KEY = 'AKIAQC3VOUARXGGMWHHO'
 SECRET_KEY = '7QbU0RSsSquT9Hv3SGO9VJIBfINysxmRjadiqGsS'
 DUMMY_SSID = '0'
 DUMMY_PSWD = '0'
-BUCKET_URL = 'http://stm32u5-dashboard.s3-website-us-west-1.amazonaws.com'
+BUCKET_URL = 'https://main.d3mkj47qkab3qo.amplifyapp.com'
+
+if platform.system() == 'Windows': 
+    BIN_FILE = '.\\bin\\b_u585i_iot02a_ntz.bin'
+else:
+    BIN_FILE = './bin/b_u585i_iot02a_ntz.bin'
+
 
 HELP = ['openDashboard.py options:', 
         '\n\t-h or --help for help',
         '\n\t-i for interactive mode',
         '\n\t--ssid=[WiFi SSID]', 
         '\n\t--password=[WiFi Password]',
-        '\n\t--device-name=[Device Name]',
-        '\n\t--key=[AWS Access Key]',
-        '\n\t--secret-key=[AWS Secret Key]',
-        '\n\t--url=[Website URL]']
+        '\n\t--aws-region=[AWS_REGION]',
+        '\n\t--aws-access-key-id=[AWS Access Key]',
+        '\n\t--aws-access-key-secret=[AWS Secret Key]']
 
 # Run path in command line and output it to output.txt if logging level is greater than debug
 def cmd(path: list):
@@ -53,12 +59,9 @@ def getHiddenParam(curParam, label):
         return curParam
 
 
-
 def main(argv):
-    
-
     try:
-        opts, args = getopt.getopt(argv,"hi", ["help", "interactive", "ssid=", "password=", "name=", "device-name=", "key=", "secret-key=", "url="])
+        opts, args = getopt.getopt(argv,"hi", ["help", "interactive", "ssid=", "password=", "key=", "secret-key="])
     except getopt.GetoptError:
         print("Parameter Error")
         sys.exit(1)
@@ -105,7 +108,7 @@ def main(argv):
         pswd = getHiddenParam(pswd, "Wi-Fi Password")
 
     
-    cmd(['python', 'utils\\flash.py'])
+    cmd(['python3', 'utils/flash.py', '--bin-file='+BIN_FILE])
     cmd(['python', 'utils\\setWiFiParam.py', '--ssid=' + DUMMY_SSID, '--password='+ DUMMY_PSWD])
     cmd(['python', 'utils\\provision.py', '--thing-name=' + name, '--wifi-ssid=' +  ssid, '--wifi-credential=' + pswd])
     cmd(['python', 'utils\\openDashboard.py', '--device-id='+ name, '--key-id='+ key,  '--secret-key='+ secretKey, '--bucket-url='+ url])
