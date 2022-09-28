710969fe6d70e9b54452ed2c37df7ad4fc210e28
SlimJallouli <slim.jallouli@st.com>
2022-09-28 14:54:44 -0700

    Fix issues with profile configuration

--- AWS_CLI_ProfileConfig.sh
+++ AWS_CLI_ProfileConfig.sh
@@ -33,25 +33,19 @@ outputForm='json'
 # Configures aws cli for prod account
 
 cat << EOF >> $HOME/.aws/config
-
 [$provision_profile]
-$region
-$outputForm
-
-[$dashboard_profile]
-$region
-$outputForm
-
+region = $region
+output = $outputForm
+[profile $dashboard_profile]
+region = $region
+output = $outputForm
 EOF
 
 cat << EOF >> $HOME/.aws/credentials
-
 [$provision_profile]
-aws_access_key_id=$provision_accessKey
-aws_secret_access_key=$provision_secretKey
-
+aws_access_key_id = $provision_accessKey
+aws_secret_access_key = $provision_secretKey
 [$dashboard_profile]
-aws_access_key_id=$dashboard_accessKey
-aws_secret_access_key=$dashboard_secretKey
-
+aws_access_key_id = $dashboard_accessKey
+aws_secret_access_key = $dashboard_secretKey
 EOF
